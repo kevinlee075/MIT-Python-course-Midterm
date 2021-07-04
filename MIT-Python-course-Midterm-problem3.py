ebb81e2606839669fcfb96e3bbf51264a6a0ce99
kevinlee075 <kevinlee075@gmail.com>
2021-07-04 16:19:36 +0800

    Create MIT-Python-course-Midterm-problem3.py

---
+++ MIT-Python-course-Midterm-problem3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  4 16:17:24 2021
+
+@author: Lenovo
+"""
+
+def closest_power(base, num):
+    '''
+    base: base of the exponential, integer > 1
+    num: number you want to be closest to, integer > 0
+    Find the integer exponent such that base**exponent is closest to num.
+    Note that the base**exponent may be either greater or smaller than num.
+    In case of a tie, return the smaller value.
+    Returns the exponent.
+    '''
+    expon = 0
+    while base ** expon < num:
+        expon += 1
+    if abs(num - base ** expon) >= abs(num - base ** (expon-1)):
+        result = expon - 1
+    else:
+        result = expon
+    return result
